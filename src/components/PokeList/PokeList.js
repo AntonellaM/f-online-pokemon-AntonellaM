b47d892b43a549a546a8ebcccf86c049ec5f61fe
Antonella Michelli <antonella.michelli@upplication.com>
2019-06-21 13:06:37 +0200

    get evolution origin

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -3,29 +3,43 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './PokeList.scss';
 
-const PokeList = (props) => {
-  return (
-    <main>
-      <section className="pokemon-results">
-        <ul className="pokemon-results__list">
-          {props.list.map((pokemon, index) =>
-            <Link key={index} className="pokemon-card__link" to={`/pokemon/${pokemon.id}`}>
-              <li className="pokemon-card" >
-                <div className="pokemon-card__header">
-                  <div className="photo" style={{backgroundImage: `url(${pokemon.sprites.front_default})`}}></div>
-                  <p>ID/{pokemon.id}</p>
-                </div>
-                <h1 className="pokemon-card__name">{pokemon.forms[0].name}</h1>
-                <div className="pokemon-card__types">
-                {pokemon.types.map((type, index) => <p key={index} className="type">{type.type.name}</p>)}
-                </div>
-              </li>
-            </Link>
-          )}
-        </ul>
-      </section>
-    </main>
-  );
+class PokeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getPokemonEvolution = this.getPokemonEvolution.bind(this);
+  }
+
+  getPokemonEvolution(URL) {
+    fetch(URL).then(res => res.json()).then(data => data.evolves_from_species !== null ? console.log(data.evolves_from_species.name) : console.log('vacío'))
+  }
+
+  render() {
+    return (
+      <main>
+        <section className="pokemon-results">
+          <ul className="pokemon-results__list">
+            {this.props.list.map((pokemon, index) =>
+              <Link key={index} className="pokemon-card__link" to={`/pokemon/${pokemon.id}`}>
+                <li className="pokemon-card" >
+                  <div className="pokemon-card__header">
+                    <div className="photo" style={{backgroundImage: `url(${pokemon.sprites.front_default})`}}></div>
+                    <p>ID/{pokemon.id}</p>
+                  </div>
+                  <h1 className="pokemon-card__name">{pokemon.forms[0].name}</h1>
+                  <div className="pokemon-card__types">
+                  {pokemon.types.map((type, index) => <p key={index} className="type">{type.type.name}</p>)}
+                  </div>
+                  <div className="pokemon-card__evolutions">
+                    {this.getPokemonEvolution(pokemon.species.url)}
+                  </div>
+                </li>
+              </Link>
+            )}
+          </ul>
+        </section>
+      </main>
+    );
+  }
 }
 
 PokeList.propTypes = {
